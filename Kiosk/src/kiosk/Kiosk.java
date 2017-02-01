e340fe2cb7127be760069ae0c538562b662d21c6
Elliott Rarden <Elliott.Rarden@mines.sdsmt.edu>
2017-01-31 17:26:26 -0700

    Created NetBeans Project

---
+++ Kiosk/src/kiosk/Kiosk.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kiosk;
+
+/**
+ *
+ * @author 7210579
+ */
+public class Kiosk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MasterWindow window = new MasterWindow();
+    }
+    
+}
