d2ba342c29385473c0402c382d6c9faa0130db23
Brady <brady@localhost.localdomain>
2017-02-04 13:29:15 -0700

    Setting base of Main Window, added <press escape to close window> and playing with LayoutManagers

--- Kiosk/src/kiosk/Kiosk.java
+++ Kiosk/src/kiosk/Kiosk.java
@@ -7,14 +7,39 @@
 package kiosk;
 
 import javax.swing.*;
+import java.awt.event.*;
 import java.awt.*;
 
 /**
  *
  * @author 7210579
  */
-public class Kiosk {
+public class Kiosk extends JFrame
+{   
+    public Kiosk()
+    {
+        super("Welcome to Mammoth Site");
+        addKeyListener( new JComponentCloseOnEscapeKey() );
+        
+        Container content = getContentPane();
+        
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Some Text");
+        panel.add(label);
+        
+        content.setLayout(new FlowLayout() );
+        content.add( CreateMainKioskScrollPane() );
+        content.add(panel);
+        
+        setContentPane(content);
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setSize(700, 600);      //set Width, Height
 
+        setVisible(true);
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -23,14 +48,29 @@ public class Kiosk {
         // TODO code application logic here
         SwingUtilities.invokeLater(new Runnable() 
         {
-            public void run(){ CreateAndShowGUI(); } 
+            public void run()
+            { 
+                CreateAndShowGUI();
+            } 
         });
     }
     
     private static void CreateAndShowGUI()
     {
-        //MasterWindow window = new MasterWindow();
-        //window.setVisible(true);
+       Kiosk kiosk = new Kiosk();
     }
     
+    private static void Exit(Kiosk kiosk)
+    {
+        kiosk.dispose();
+    }
+    
+    private JScrollPane CreateMainKioskScrollPane()
+    {
+        JScrollPane pane = new JScrollPane();
+        pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        pane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        
+        return pane;
+    }
 }
